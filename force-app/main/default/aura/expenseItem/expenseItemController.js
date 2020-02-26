671b7299f6f53b99ad081482c1817bde929aa901
kbcarte <kbcarte@gmail.com>
2020-02-26 13:39:05 -0500

    added list component

--- force-app/main/default/aura/expenseItem/expenseItemController.js
+++ force-app/main/default/aura/expenseItem/expenseItemController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+    doInit : function(component, event, helper) {
+        var mydate = component.get("v.expense.Date__c");
+        if(mydate){
+            component.set("v.formatdate", new Date(mydate));
+        }
+    },
 })

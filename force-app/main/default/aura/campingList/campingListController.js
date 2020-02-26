dff3bee98191633a6495f4f919ee8ad6dd7fc7b1
kbcarte <kbcarte@gmail.com>
2020-02-26 15:10:12 -0500

    added server-side controller

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,22 +1,30 @@
 ({
-    clickCreateItem : function(component, event, helper) {
-        var validCamping = component.find('campingform').reduce(function (validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
+    doInit  : function(component, event, helper) {
+		var action = component.get("c.getItems");
+        action.setCallback(this, function(response){
+            var state = response.getState();
 
-        if(validCamping){
-            var newCampingItem = component.get("v.newItem");
-            //helper.createCamping(component,newCampingItem);
-            var campings = component.get("v.items");
-            var item = JSON.parse(JSON.stringify(newCampingItem));
+            if (component.isValid() && state === "SUCCESS") {
 
-            campings.push(item);
 
-            component.set("v.items",campings);
-            component.set("v.newItem",{ 'sobjectType': 'Camping_Item__c','Name': '','Quantity__c': 0,
-            'Price__c': 0,'Packed__c': false });
+                component.set("v.items", response.getReturnValue());
+
+            }
+        });
+
+        $A.enqueueAction(action);
+	},
+
+    createItem : function(component, event, helper){
+
+        helper.validateFields (component,component.find("name"));
+        helper.validateFields (component,component.find("Price"));
+        helper.validateFields (component,component.find("Quantity"));
+        if(component.get("v.er") === false)
+        {
+            var Item = component.get("v.newItem");
+            helper.createItem (component,Item);
+
         }
-    }
+	}
 })

a989ae89c7df5429beccbcae666bd4690a9a1880
kbcarte <kbcarte@gmail.com>
2020-02-26 13:33:49 -0500

    added helper function, added new component expenseItem

--- force-app/main/default/aura/expenses/expensesHelper.js
+++ force-app/main/default/aura/expenses/expensesHelper.js
@@ -1,5 +1,12 @@
 ({
-    helperMethod : function() {
+    createExpense: function(component, expense) {
+        var theExpenses = component.get("v.expenses");
 
+        // Copy the expense to a new object
+        // THIS IS A DISGUSTING, TEMPORARY HACK
+        var newExpense = JSON.parse(JSON.stringify(expense));
+
+        theExpenses.push(newExpense);
+        component.set("v.expenses", theExpenses);
     }
 })

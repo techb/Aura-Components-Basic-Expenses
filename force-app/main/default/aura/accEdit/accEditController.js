40dcada122af2f6e7dbe26bf37ff88f72d903397
kbcarte <kbcarte@gmail.com>
2020-02-27 10:59:23 -0500

    Handle Record Changes and Errors challenge complete

--- force-app/main/default/aura/accEdit/accEditController.js
+++ force-app/main/default/aura/accEdit/accEditController.js
@@ -6,6 +6,11 @@
                 console.log("User is offline, device doesn't support drafts.");
             } else if (saveResult.state === "ERROR") {
                 console.log('Problem saving record, error: ' + JSON.stringify(saveResult.error));
+                var err = "";
+                for (var i = 0; i < saveResult.error.length; i++) {
+                    err += saveResult.error[i].message + "\n";
+                }
+                component.set("v.recordSaveError", err);
             } else {
                 console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
             }
